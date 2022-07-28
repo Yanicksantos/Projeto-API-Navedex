87aa06eb6bfeeffa349057de76b83f0a1b2bd6e7
Yanick Santos <yanickeduardo04@outlook.com>
2022-07-28 00:26:50 -0300

    sicronizando a tela com a api

--- src/scripts/home.js
+++ src/scripts/home.js
@@ -4,6 +4,7 @@ const api = {
     url:"https://navedex-api.herokuapp.com/v1/navers"
 }
 
+let controle_modal = false
 async function preencherNavers(){
     const usuario =JSON.parse(localStorage.getItem("_usuario_"))
     if(usuario==null){
@@ -21,7 +22,7 @@ async function preencherNavers(){
         console.log(navers)
         if(response.ok){
             for(let i=0; i<navers.length; i++){
-                $navers_home.innerHTML+=`<div class="navers"><div class="img_home"><div class="view_details"><div class="view">Ver mais...</div></div><img class="img_home" src="https://thumbs.dreamstime.com/b/desenho-animado-do-perfil-executivo-funcion%C3%A1rio-de-escrit%C3%B3rio-da-empresa-ilustra%C3%A7%C3%A3o-vetorial-oper%C3%A1ria-avatar-no-estilo-224793366.jpg" alt=""></div><div class="info_navers"><div class="name">${navers[i].name}</div><div class="profission">${navers[i].job_role}</div></div><div class="options_home"><i class="fa-solid fa-trash delete"></i><i class="fa-solid fa-pen put"></i></div></div>`
+                $navers_home.innerHTML+=`<div class="navers"><div class="img_home"><div class="view_details"><div class="view" id="${i}">Ver mais...</div></div><img class="img_home" src="https://thumbs.dreamstime.com/b/desenho-animado-do-perfil-executivo-funcion%C3%A1rio-de-escrit%C3%B3rio-da-empresa-ilustra%C3%A7%C3%A3o-vetorial-oper%C3%A1ria-avatar-no-estilo-224793366.jpg" alt=""></div><div class="info_navers"><div class="name">${navers[i].name}</div><div class="profission">${navers[i].job_role}</div></div><div class="options_home"><i class="fa-solid fa-trash delete" id="${i+10}"></i><i class="fa-solid fa-pen put" id="${i-10}"></i></div></div>`
             }
             const $view = document.querySelectorAll(".view")
             const $exit_modal =document.querySelector("#exit_modal")
@@ -36,25 +37,30 @@ async function preencherNavers(){
             const $add = document.querySelector(".add")
 
             $view.forEach(element => {
-                element.addEventListener("click", () => {
+                element.addEventListener("click", (events) => {
+                    console.log(events.target)
+                    const id = parseInt(events.target.id)
                     $excluir_modal.style.display="none"
                     $info_naver.style.display="none"
+                    preencherNaver(id)
                     opennmodal()
                     $modal_details.style.display="flex"
                 })
             });
 
-            $put.forEach(put => {
-                put.addEventListener("click", () => {
-                   editar()
-                })
-            });
 
             $put.forEach(put => {
-                put.addEventListener("click", () => {
-                   editar()
+                put.addEventListener("click", (events) => {
+                    console.log(events.target)
+                    if(events.target.classList.contains("put_modal")){
+                       editar(events.target.id)
+                    }else{
+                        editar(navers[(parseInt(events.target.id))+10].id)
+                    }
+        
                 })
             });
+            
 
 
             document.onclick = function(event) {
@@ -66,20 +72,26 @@ async function preencherNavers(){
 
 
             $delete.forEach(del => {
-                del.addEventListener("click", () => {
+                del.addEventListener("click", (events) => {
+                    /*console.log(events.target)
+                   */
+                    console.log(events.target)
                     $exit_modal.style.display="none"
                     $modal_details.style.display="none"
                     $info_naver.style.display="none"
                     opennmodal()
                     $excluir_modal.style.display="block"
-
                 })
             });
 
 
 
 
-            $button_exluir.addEventListener("click", ()=> {
+            $button_exluir.addEventListener("click", (event)=> {
+                console.log(event.target.id)
+                if(event.target.classList.contains("del_modal")){
+                    console.log(event.target.id)
+                 }
                 $modal_details.style.display="none"
                 $excluir_modal.style.display="none"
                 opennmodal()
@@ -90,14 +102,28 @@ async function preencherNavers(){
 
             $cancelar.addEventListener("click", ()=> $open_modal.style.display="none")
             $exit_modal.addEventListener("click", ()=> $open_modal.style.display="none")
-            function opennmodal(){document.querySelector(".open_modal").style.display="flex"}
-            function editar(){
+            function opennmodal(){
+                document.querySelector(".open_modal").style.display="flex"
+                controle_modal = true
+                return controle_modal
+            }
+            function editar(id){
                 $exit_modal.style.display="none"
                 $modal_details.style.display="none"
                 $info_naver.style.display="none"
                 $open_modal.style.display="none"
                 $excluir_modal.style.display="block"
-                location.href=`editar.html?nome=YanickSantos&profissao=Front-end Developer&idade=28&tempo=50&projeto=nao sei onde está o erro`;
+                location.href=`editar.html?id=${id}`;
+            }
+
+            function preencherNaver(id){
+                document.querySelector(".name_modal").textContent=`${navers[id].name}`
+                document.querySelector(".profission_modal").textContent=`${navers[id].job_role}`
+                document.querySelector(".idade_modal").textContent=`${navers[id].birthdate}`
+                document.querySelector(".tempo_empresa_modal").textContent=`${navers[id].admission_date}`
+                document.querySelector("#Projet").textContent=`${navers[id].project}`
+                $button_exluir.id=`${navers[id].id}`
+                document.querySelector(".put_modal").id=`${navers[id].id}`
             }
         }else{
             alert("Ocorreu algum erro! Tente reiniciar...")
